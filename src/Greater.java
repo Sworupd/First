06264b8836170f4f6beb846f3d1d2c8cf3590017
LENOVO <sworupdhakal88@gmail.com>
2024-07-09 13:52:35 +0545

    j

---
+++ src/Greater.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Greater {
+    public static void main(String[] args) {
+        int num1, num2, num3;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number: ");
+        num1 = sc.nextInt();
+        System.out.print("Enter Second Number: ");
+        num2 = sc.nextInt();
+        System.out.print("Enter Third Number: ");
+        num3 = sc.nextInt();
+        if(num1 > num2 && num1 > num3){
+            System.out.println("Greater Number is " +num1);
+        }
+        else if(num2 > num1 && num2 > num3){
+            System.out.println("Greater Number is " +num2);
+        }
+        else{
+            System.out.println("Greater Number is " +num3);
+        }
+    }
+}
