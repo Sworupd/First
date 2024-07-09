19224e736f3ed79416b71d6c9184fad428a1cbb7
LENOVO <sworupdhakal88@gmail.com>
2024-07-09 14:14:51 +0545

    j

--- src/Election.java
+++ src/Election.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 public class Election {
     public static void main(String[] args) {
         int age;
+        String citizen;
         Scanner elect = new Scanner(System.in);
         System.out.println("Enter your age: ");
         age = elect.nextInt();
-        if (age >= 18) {
+        System.out.println("Are you a citizen of Nepal?");
+        citizen = elect.next();
+        if (age >= 18 && citizen.equals("Yes")) {
             System.out.println("You are able to vote in Nepal");
         }
         else{
